4d3c73dd3ab1c47656360f9969fb97c423b62411
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2024-03-02 12:19:43 -0300

    mudando o http para https

--- src/services/Requisicoes_Api.js
+++ src/services/Requisicoes_Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function buscarListaCandidato() {
   try {
-    const response = await axios.get('http://api-eleicoes-production.up.railway.app/candidatos/listar');
+    const response = await axios.get('https://api-eleicoes-production.up.railway.app/candidatos/listar');
     return response.data;
   } catch (error) {
     console.error('Erro na requisição:', error);
@@ -12,7 +12,7 @@ export async function buscarListaCandidato() {
 
 export async function buscarListaCargo() {
   try {
-    const response = await axios.get('http://api-eleicoes-production.up.railway.app/cargos/listar');
+    const response = await axios.get('https://api-eleicoes-production.up.railway.app/cargos/listar');
     return response.data;
   } catch (error) {
     console.error('Erro na requisição:', error);
@@ -22,7 +22,7 @@ export async function buscarListaCargo() {
 
 export async function buscarVotosCandidatosCargos() {
   try {
-    const response = await axios.get('http://api-eleicoes-production.up.railway.app/votos/relatorio/candidatos-Cargos');
+    const response = await axios.get('https://api-eleicoes-production.up.railway.app/votos/relatorio/candidatos-Cargos');
     return response.data;
   } catch (error) {
     console.error('Erro na requisição:', error);
@@ -32,7 +32,7 @@ export async function buscarVotosCandidatosCargos() {
 
 export async function criarVoto(obj, id) {
   try {
-    const response = await axios.post(`http://api-eleicoes-production.up.railway.app/votos/eleitor/${id}`, obj, {
+    const response = await axios.post(`https://api-eleicoes-production.up.railway.app/votos/eleitor/${id}`, obj, {
       headers: {
         'Content-Type': 'application/json',
       },
@@ -59,7 +59,7 @@ export async function buscarMunicipios(uf) {
 
 export async function criarEleitor(obj) {
   try {
-    const response = await axios.post(`http://api-eleicoes-production.up.railway.app/eleitores`, obj, {
+    const response = await axios.post(`https://api-eleicoes-production.up.railway.app/eleitores`, obj, {
       headers: {
         'Content-Type': 'application/json',
       },
